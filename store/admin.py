af58e899cdd2ff30cfe946c4564499c340d88cc8
Tim <qitingliu7@gmail.com>
2021-08-26 09:46:33 -0400

    Overriding the base queryset: customer

--- store/admin.py
+++ store/admin.py
@@ -24,12 +24,27 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ["first_name",'last_name','membership']
+    list_display = ["first_name",'last_name','membership','orders']
     list_editable = ['membership']
     ordering = ['first_name','last_name']
     list_per_page = 10
     # search startswith
     search_fields = ['first_name__istartswith','last_name__istartswith']
+
+    @admin.display(ordering='orders_count')
+    def orders(self, customer):
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({
+                'customer__id': str(customer.id)
+            }))
+        return format_html('<a href="{}">{} Orders</a>', url, customer.orders_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
