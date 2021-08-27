4a7b20d85e9c5588c30140bbce43132454d9f7ed
Tim <qitingliu7@gmail.com>
2021-08-27 15:49:31 -0400

    Adding data validation

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 
@@ -13,25 +14,31 @@ class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
     def __str__(self):
         return self.title
+
     class Meta:
         ordering = ['title']
+
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    inventory = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
+    unit_price = models.DecimalField(max_digits=6,
+                                     decimal_places=2,
+                                     validators=[MinValueValidator(1)])
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
     last_update = models.DateTimeField(auto_now=True)
     # When delete a collection, do not delete a product
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(promotion)
-    
+    promotions = models.ManyToManyField(promotion, blank=True)
+
     # show product title at admin site
     def __str__(self):
-      return self.title
+        return self.title
+
     class Meta:
         ordering = ['title']
 
@@ -53,7 +60,7 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
-    
+
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
